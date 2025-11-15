fcd72be265939587029404cd9cd8147f97051cc3
Mihuil121 <bezugliymicha@gmail.com>
2025-11-15 10:50:06 +0300

    chore: remove accidentally committed browser files

--- src/image/photo ireland - Поиск в Google_files/m=KiuZBf.js
+++
@@ -1,6 +0,0 @@
-loaded_h_0(function(_){var window=this;
-_.t("KiuZBf");
-_.Oe(_.L1a,class extends _.co{static Sa(){return{service:{XC:_.Lu}}}constructor(a){super();this.XC=a.service.XC}isAvailable(){return this.XC.isAvailable()}isInBasicMode(){return this.XC.isInBasicMode()}async enterBasicMode(a){try{_.y(await _.y(this.XC.enterBasicMode(a)))}finally{_.y()}}async exitBasicMode(a){try{_.y(await _.y(this.XC.exitBasicMode(a)))}finally{_.y()}}async restoreToPreviousState(a){try{_.y(await _.y(this.XC.restoreToPreviousState(a)))}finally{_.y()}}});
-_.v();
-});
-// Google Inc.
--- src/image/photo ireland - Поиск в Google_files/m=uveYne.js
+++
@@ -1,7 +0,0 @@
-"use strict";loaded_1_11(function(_){var window=this;
-_.k("uveYne");
-var ywm=function(a,b,c){a.sessionStorage.set(b,c)},zwm=class{constructor(){this.cache=new Set}},Awm=new zwm;_.Bwm=class extends _.Yu{static Za(){return{model:{Rm:_.fy},service:{oXa:_.Pae,Ml:_.Tx,zy:_.k8d}}}constructor(a){super();this.oa=_.ey(a.model.Rm);this.Ml=a.service.Ml;this.Ja=a.service.oXa}La(a){const b=this.Ka().wb();ywm(this.Ja,this.oa,this.Ea());Awm.cache.has(this.oa)?b.style.animation="none":(Awm.cache.add(this.oa),b.addEventListener("animationend",()=>{b.style.animation="none"}),b.addEventListener("animationcancel",()=>{b.style.animation="none"}));a.data.jCb&&_.GRb(this.Ml,this.oa)}Ea(){const a=
-{};var b=_.Lu(_.Mu(this.Ka(),"N9Xkfe"));for(const c of b){b=c.dataset.ved;const d=c.dataset.tbnid;b&&d&&(a[d]=b)}return a}Ga(a){_.j8d(a.targetElement)}Aa(){}};_.Bwm.prototype.$wa$NMdhcc=function(){return this.Aa};_.Bwm.prototype.$wa$SjNcmb=function(){return this.Ga};_.Bwm.prototype.$wa$Rq6fF=function(){return this.Ea};_.Bwm.prototype.$wa$DmdRif=function(){return this.La};_.av(_.K6a,_.Bwm);
-_.l();
-});
-// Google Inc.
